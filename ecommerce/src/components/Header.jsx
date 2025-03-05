6969c6738122fb8b7193e7d7c86460a278cb2f76
ThamaraiselviR21 <thamaraiselvir16@gmail.com>
2025-03-05 04:39:56 -0800

    design

--- ecommerce/src/components/Header.jsx
+++ ecommerce/src/components/Header.jsx
@@ -1,7 +1,23 @@
 import { Link } from 'react-router-dom'
 import Search from './Search'
+import { useState } from 'react';
 import './header.css'
 export default function Header({cartItems}) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleNavbar = () => {
+    setIsOpen(!isOpen);
+  };
+  const scrolla = () => {
+    document.getElementById('aboutus')?.scrollIntoView({ behavior: 'smooth' });
+ }
+ const scrollc = () => {
+  document.getElementById('contactus')?.scrollIntoView({ behavior: 'smooth' });
+}
+const scrollToProducts = () => {
+  document.getElementById('products_heading')?.scrollIntoView({ behavior: 'smooth' });
+}
+
   return (
     // <div>
     //       <nav className="navbar row">
@@ -26,28 +42,74 @@ export default function Header({cartItems}) {
     // </nav>
       
     // </div>
-    <div className="bg-gray-900 text-white shadow-lg fixed top-0 left-0 w-full z-50 ">
-      <nav className="flex flex-col md:flex-row items-center justify-between  px-5 max-w-7xl mx-auto">
+    <>
+    <div className="hidden sm:block bg-gray-900 text-white shadow-lg fixed top-0 left-0 w-full z-50 ">
+
+      <nav className="flex flex-col md:flex-row items-center  justify-between py-0 px-5 max-w-7xl mx-auto">
         {/* Logo */}
         <div className="flex-shrink-0">
           <img className="w-28" src="/images/l1 (2).png" alt="Logo" />
         </div>
 
         {/* Search Bar */}
-        <div className="relative w-full md:w-1/3 mt-4 md:mt-0">
-          <Search className="w-full border-2 border-pink-400 p-3 rounded-lg outline-none focus:ring-2 focus:ring-pink-500 transition duration-300" />
-        </div>
-
-        {/* Cart Link */}
-        <div className="mt-4 md:mt-0">
-          <Link to="/cart" className="flex items-center space-x-2 text-pink-400 hover:text-pink-600 transition font-semibold">
-            <span>Cart</span>
-            <span className="bg-pink-500 text-white px-3 py-1 rounded-full text-sm">{cartItems.length}</span>
+        <div className="relative w-full md:w-1/2 mt-4 md:mt-0">
+          <Search className="w-full border-2 border-pink-400  rounded-lg outline-none focus:ring-2 focus:ring-pink-500 transition duration-300" />
+        </div> 
+         {/* Cart Link */}
+         <div className="mt-4 md:mt-0">
+        <Link to="/cart" className="btn p-2 position-relative border-2 border-pink-500 bg-white ">
+          <i className="fa-solid fa-cart-shopping"></i>
+            <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">{cartItems.length}</span>
           </Link>
         </div>
+        <div className="mt-4 md:mt-0">
+        <button onClick={scrollc} className="btn p-2 position-relative border-2 border-pink-500 bg-white ">
+        <i className="fa-solid fa-phone-volume"></i>
+          </button>
+        </div>
+        <div className="mt-4 md:mt-0">
+        <button onClick={scrolla} className="btn p-2 position-relative border-2 border-pink-500 bg-white ">
+        <i className="fa-solid fa-users"></i>
+          </button>
+        </div>
+        
+        
       </nav>
     </div>
+    <div className="relative sm:hidden fixed top-0 left-0 bg-gray-900   "> {/* Only visible on small screens */}
+    <nav className=" shadow-md p-4">
+      {/* Menu Button (Hamburger) */}
+      <div className='flex justify-between'>
+         <div className="menu-btn text-2xl cursor-pointer" onClick={toggleNavbar}>
+         <i className="fa-solid fa-bars text-pink-500 " ></i>
+         </div>
+          <div className=" md:mt-0">
+          <Link to="/cart" className="btn p-2 position-relative border-2 border-pink-500 bg-pink-300 ">
+          <i className="fa-solid fa-cart-shopping"></i>
+            <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">{cartItems.length}</span>
+          </Link>
+        </div>
+      </div>
+      <div className="relative w-full md:w-1/3 mt-4 md:mt-0">
+          <Search className="w-full border-2 border-pink-400 p-3 rounded-lg outline-none focus:ring-2 focus:ring-pink-500 transition duration-300" />
+        </div>
+      
+      {/* Sidebar */}
+      <div className={`fixed inset-y-0 left-0 w-64 bg-pink-300 shadow-lg transform transition-transform duration-300 ease-in-out z-50 ${isOpen ? 'translate-x-0' : '-translate-x-full'}`}>
+        {/* Close Button */}
+        <button className="absolute top-2 right-3 text-2xl" onClick={toggleNavbar}>×</button>
 
+        {/* Menu Items */}
+        <ul className="flex flex-col space-y-4 mt-10 text-lg ">
+          <li onClick={scrollToProducts}><a  className="block px-4 py-2 hover:bg-gray-100">Home</a></li>
+          <li onClick={scrolla}><a  className="block px-4 py-2 hover:bg-gray-100">About us</a></li>
+          <li onClick={scrollc}><a  className="block px-4 py-2 hover:bg-gray-100">Contact</a></li>
+        </ul>
+      </div>
+    </nav>
+  </div>
+  </>
+  
    
      
   )
--- ecommerce/src/pages/Home.jsx
+++ ecommerce/src/pages/Home.jsx
@@ -33,9 +33,34 @@ function Home() {
      {product.map((product, index) => (
   <Productsitems key={index} product={product} />
     ))}
-
    </div>
     </section>
+    <div className="bg-gray-500 rounded-2xl m-2"id="aboutus"  >
+      <h1 className="text-center p-2" >About Us</h1>
+       <h4 className="px-5 text-white tracking-wide italic text-xl">Welcome to <b className="text-pink-500">LOTUS TRENDS</b>, your one-stop destination for high-quality products and a seamless online shopping experience.We started with a simple goal — to provide our customers with the best products at affordable prices, delivered right to their doorstep. From trendy fashion and accessories to daily essentials, we carefully curate our collections to meet your needs and keep up with the latest styles.</h4><br/>
+       
+       <h3 className=" text-pink-700 text-center text-xl ">Why choose us?</h3>
+       <div className="flex flex-col m-2">
+       <h4 className="  text-center text-justify p-2 font-mono "><b className="text-black">Quality Products : </b>We believe in offering only the best.</h4>
+       <h4 className="  text-center p-2 font-mono " ><b className="text-black">Customer Satisfaction : </b>Your happiness is our priority.</h4>
+       <h4 className="  text-center p-2 font-mono"><b className="text-black">Secure Shopping : </b>Shop with confidence through our safe and trusted platform.</h4>
+       <h4 className=" text-center p-2 font-mono"><b className="text-black" >Fast Delivery : </b>Get your orders quickly and on time.</h4><br/>
+       </div>
+       <h4 className=" px-5 text-white tracking-wide italic text-xl">Thank you for choosing <b className="text-pink-500">LOTUS TRENDS</b>. We’re excited to be a part of your shopping journey and look forward to serving you! <br/>For any questions, feel free to contact us. We're always here to help.</h4>
+    </div>
+    <div className="bg-gray-500 rounded-2xl m-2  " id="contactus">
+      <h1 className="text-center p-2" >Contact Us</h1>
+      <h4 className="px-5 text-white tracking-wide italic text-xl">We’re always happy to help! If you have any questions, suggestions, or need support with your order, feel free to reach out. Our team will get back to you as soon as possible.</h4>
+      <h2 className="font-mono text-3xl text-center text-black">Contact Details:</h2>
+      <div className=" m-2 text-center py-3">
+        <h3 className="font-mono text-2xl"><i className="fa-solid fa-envelope-circle-check"></i>  Email : </h3><h4 className="text-blue-500 font-mono text-base">lotustrends@gmail.com</h4>
+        <h3 className="font-mono text-2xl"><i className="fa-solid fa-phone"></i>  Phone : </h3> <h4 className="text-blue-500 font-mono text-base" > +91 6787978890</h4>
+        <h3 className="font-mono text-2xl"><i className="fa-solid fa-location-dot"></i> Address : </h3><h4 className="text-white font-mono text-base">lotus Trends
+   5678 Commerce Avenue, <br/>Suite 102
+   Brooklyn Heights, NY 11201
+   United States</h4>
+      </div>
+    </div>
 
     <footer className="py-1 bg-gray-900">
       <p className="text-center text-white mt-1 "> &copy; ecommerce
